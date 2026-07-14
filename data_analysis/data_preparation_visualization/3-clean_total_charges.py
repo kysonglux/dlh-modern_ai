c44a33195d4c3c5fee437514161a8b4ab1660aec
kysong <kyoungran.song@ai.dlh.lu>
2026-07-14 07:18:08 +0000

    3-clean_total_charges.py

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" data cleaning using Dropping, replacing, imputation """
+
+import pandas as pd
+
+
+def clean_total_charges(df, method='drop'):
+    """data cleaning using Dropping, replacing, imputation """
+    df = df.copy()
+
+    if method == 'drop':
+        df = df.dropna(subset=['TotalCharges'])
+    elif method in ('median', 'impute'):
+        value = (
+            df['TotalCharges'].median()
+            if method == 'median'
+            else df['MonthlyCharges'] * df['tenure'])
+        df['TotalCharges'] = df['TotalCharges'].fillna(value)
+    return df
