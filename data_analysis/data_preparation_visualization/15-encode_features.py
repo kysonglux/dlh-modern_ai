4a38157c6bb784a48bddd02ed2f63cfb5eec5bbb
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 14:46:48 +0000

    15-encode_features.py

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+""" encodes features for modeling using scikit-learn"""
+
+import pandas as pd
+from sklearn import preprocessing
+
+
+def encode_features(df):
+    """encodes features for modeling using scikit-learn
+    Returns:
+    -encoded DataFrame
+    -fitted LabelEncoder for Churn
+    -fitted OrdinalEncoder for binary columns
+    -fitted OrdinalEncoder for TenureGroup
+    """
+
+    '#1. Label encode Churn'
+    churn_le = preprocessing.LabelEncoder()
+    df["Churn"] = churn_le.fit_transform(df["Churn"])
+
+    '#2. Ordinal encode binary Yes/No columns'
+    bi_cols = ["Partner", "Dependents", "PaperlessBilling", "SeniorCitizen"]
+    cats = [["No", "Yes"]]*len(bi_cols)
+    binary_oe = preprocessing.OrdinalEncoder(categories=cats)
+    df[bi_cols] = binary_oe.fit_transform(df[bi_cols])
+
+    '#3. Ordinal encode TenureGroup(alphabetical order)'
+    tenure_cats = sorted(df["TenureGroup"].unique())
+    tenure_oe = preprocessing.OrdinalEncoder(categories=[tenure_cats])
+    df["TenureGroup"] = tenure_oe.fit_transform(df[["TenureGroup"]])
+
+    '#4.One-hot encode Contract and paymentMethod (drop first)'
+    df = pd.get_dummies(
+        df,
+        columns=["Contract", "PaymentMethod"],
+        drop_first=True)
+
+    return df, churn_le, binary_oe, tenure_oe
