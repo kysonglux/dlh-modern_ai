6fbfc4ef1e037b026351c5ce3d6a9ed90a6d266a
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 05:11:36 +0000

    7-plot_categorical_distributions.py

--- data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
+++ data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
@@ -16,8 +16,8 @@ def plot_categorical_distributions(df, columns_to_plot=None):
             col for col in columns_to_plot
             if col in df.columns and df[col].dtype == 'object'
         ]
-    n_cols = 2
-    n_rows = (len(columns_to_plot) + 1) // 2
+    n_cols = 3
+    n_rows = (len(columns_to_plot) + n_cols - 1) // n_cols
 
     fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5*n_rows))
 
@@ -26,10 +26,12 @@ def plot_categorical_distributions(df, columns_to_plot=None):
     for ax, col in zip(axes, columns_to_plot):
         value_counts = df[col].value_counts()
 
-        ax.bar(value_counts.index, value_counts.values)
+        positions = range(len(value_counts))
+
+        ax.bar(positions, value_counts.values)
         ax.set_title(col)
 
-        ax.set_xticks(range(len(value_counts.index)))
+        ax.set_xticks(positions)
         ax.set_xticklabels(value_counts.index, rotation=45, ha='right')
 
     for i in range(len(columns_to_plot), len(axes)):
