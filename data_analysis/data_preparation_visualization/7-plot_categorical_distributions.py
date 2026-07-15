034a470e1373be7a710ee6681a8a4b0d256c3d84
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 05:30:57 +0000

    7-plot_categorical_distributions.py

--- data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
+++ data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
@@ -6,36 +6,34 @@ import matplotlib.pyplot as plt
 
 def plot_categorical_distributions(df, columns_to_plot=None):
     """ visualizes categorical feature distributions """
+    df = df.copy()
+
     if columns_to_plot is None:
-        columns_to_plot = [
-            col for col in df.columns
-            if df[col].dtype == 'object' and col != "Churn"
-        ]
+        df = df.drop(columns=['Churn'], errors='ignore')
+        df = df.select_dtypes(include=['object'])
     else:
-        columns_to_plot = [
-            col for col in columns_to_plot
-            if col in df.columns and df[col].dtype == 'object'
-        ]
-    n_cols = 3
-    n_rows = (len(columns_to_plot) + n_cols - 1) // n_cols
+        df = df[columns_to_plot]
+
+    total_columns = df.shape[1]
+    n_cols, n_rows = (3, (total_columns + 3 - 1) // 3)
 
     fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5*n_rows))
 
     axes = axes.flatten()
 
-    for ax, col in zip(axes, columns_to_plot):
-        caategories = df[col].dropna().unique()
+    for ax, col in zip(axes, df.columns):
+        categories = df[col].dropna().unique()
         counts = df[col].value_counts().reindex(categories, fill_value=0)
 
-        positions = range(len(value_counts))
+        positions = range(len(categories))
 
-        ax.bar(positions, value_counts.values)
+        ax.bar(positions, counts.values)
         ax.set_title(col)
 
         ax.set_xticks(positions)
         ax.set_xticklabels(categories, rotation=45, ha='right')
 
-    for i in range(len(columns_to_plot), len(axes)):
+    for i in range(total_columns, len(axes)):
         axes[i].axis('off')
 
     plt.tight_layout()
