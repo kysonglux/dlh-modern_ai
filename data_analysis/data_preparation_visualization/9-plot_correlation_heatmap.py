241d6ddc2afe8009895aa689e37e81969eae1d17
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 07:31:43 +0000

    9-plot_correlation_heatmap.py

--- data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""visualizes correlations between continuous numeric features"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """visualizes correlations between continuous numeric features"""
+    plt.figure(figsize=(6, 5))
+
+    numeric_df = df.select_dtypes(include=['number'])
+    corr = numeric_df.corr()
+
+    sns.heatmap(
+        corr,
+        annot=True,
+        cmap="coolwarm",
+        vmin=-1,
+        vmax=1
+    )
+
+    plt.title("Correlation Matrix")
+    plt.tight_layout()
+    plt.show()
+    plt.savefig("Task_9.png")
