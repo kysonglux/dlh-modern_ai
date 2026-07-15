e97da6d5f073f8509e2da83e891467aeade3b379
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 09:38:06 +0000

    12-chi_square_tests.py

--- data_analysis/data_preparation_visualization/12-chi_square_tests.py
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" chi-square tests for categorical features """
+
+import pandas as pd
+from scipy import stats
+
+
+def chi_square_tests(df):
+    """chi-square tests for categorical features"""
+    results = {}
+
+    categorical_cols = df.select_dtypes(include=["object"]).columns
+    categorical_cols = [col for col in categorical_cols if col != "Churn"]
+
+    for col in categorical_cols:
+        contigency_table = pd.crosstab(df[col], df["Churn"])
+        chi2, p_value, _, _ = stats.chi2_contingency(contigency_table)
+        results[col] = float(p_value)
+    return results
