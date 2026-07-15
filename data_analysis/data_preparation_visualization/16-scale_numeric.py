1e0e54e1af4f424f746e01d0df53f46252c66d92
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 16:03:00 +0000

    16-scale_numeric.py

--- data_analysis/data_preparation_visualization/16-scale_numeric.py
+++ data_analysis/data_preparation_visualization/16-scale_numeric.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""standardizes numeric columns"""
+
+from sklearn import preprocessing
+
+
+def scale_numeric(df):
+    """ Standardizes numeric columns"""
+
+    scaler = preprocessing.StandardScaler()
+
+    cols = ["MonthlyCharges", "TotalCharges"]
+
+    df[cols] = scaler.fit_transform(df[cols])
+
+    return df
