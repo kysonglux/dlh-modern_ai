37ee9bea7e72ee52a0bf0ee91807622b18f04f2d
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 10:18:27 +0000

    13-ttest_numeric.py

--- data_analysis/data_preparation_visualization/13-ttest_numeric.py
+++ data_analysis/data_preparation_visualization/13-ttest_numeric.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""Welch's tests for continuous numeric features using scipy"""
+
+from scipy import stats
+
+
+def ttest_numeric(df):
+    """Welch:s tests for continuous numeric features using scipy"""
+    results = {}
+    numeric_cols = df.select_dtypes(include=["number"]).columns
+
+    churn_yes = df[df["Churn"] == "Yes"]
+    churn_no = df[df["Churn"] == "No"]
+
+    for col in numeric_cols:
+        yes_vals = churn_yes[col].dropna()
+        no_vals = churn_no[col].dropna()
+
+        _, p_value = stats.ttest_ind(yes_vals, no_vals, equal_var=False)
+
+        results[col] = p_value
+    return results
