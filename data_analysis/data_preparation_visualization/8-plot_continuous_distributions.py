7078c861777ab7e1fc1922cf32c3af2ca2632926
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 07:16:56 +0000

    8-plot_continuous_distributions.py

--- data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
+++ data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
@@ -12,10 +12,7 @@ def plot_continuous_distributions(df, columns_to_plot=None):
     if columns_to_plot is None:
         df = df.select_dtypes(include=[np.number])
     else:
-        df = df[[col for col in columns_to_plot
-                if col in df.colunmns
-                and np.issubdtype(df[col].dtype, np.number)]
-                ]
+        df = df[columns_to_plot]
 
     n_cols = df.shape[1]
     fig, axes = plt.subplots(n_cols, 2, figsize=(10, 3*n_cols))
@@ -26,8 +23,7 @@ def plot_continuous_distributions(df, columns_to_plot=None):
     for i, col in enumerate(df.columns):
         data = df[col].dropna().values
 
-        ax_hist = axes[i][0]
-        ax_hist.hist(
+        axes[i, 0].hist(
             data,
             bins=30,
             density=True,
@@ -36,19 +32,18 @@ def plot_continuous_distributions(df, columns_to_plot=None):
         )
 
         kde = stats.gaussian_kde(data)
-        x_vals = np.linspace(data.min(), data.max(), 500)
-        ax_hist.plot(x_vals, kde(x_vals), color='red', linestyle='--')
+        x_vals = np.linspace(data.min(), data.max(), 200)
+        axes[i, 0].plot(
+            x_vals,
+            kde(x_vals),
+            color='red',
+            linestyle='--'
+            )
 
-        ax_hist.set_title(f"{col} Histogram + KDE")
+        axes[i, 0].set_title(f"{col} Histogram + KDE")
 
-        if col == "TotalCharges":
-            max_density = kde(x_vals).max()
-            yticks = np.arange(0, max_density + 0.0004, 0.0002)
-            ax_hist.set_yticks(yticks)
-
-        ax_box = axes[i][1]
-        ax_box.boxplot(data, vert=False)
-        ax_box.set_title(f"{col} Boxplot")
+        axes[i, 1].boxplot(data, vert=False)
+        axes[i, 1].set_title(f"{col} Boxplot")
 
     plt.tight_layout()
     plt.savefig("Task_8.png")
