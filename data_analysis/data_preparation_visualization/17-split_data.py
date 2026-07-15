5a3b262b06354058232040bced9d8ded1a9f181a
kysong <kyoungran.song@ai.dlh.lu>
2026-07-15 16:05:40 +0000

    17-split_data.py

--- data_analysis/data_preparation_visualization/17-split_data.py
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" splits data into tran/test sets"""
+from sklearn import model_selection
+
+
+def split_data(df, target='Churn', test_size=0.2, random_state=42):
+    """Splits the DataFrame into train/test sets"""
+
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test
