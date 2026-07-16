4a728f49ebe1ee68f8413a79e9b4fe58eb7d5024
kysong <kyoungran.song@ai.dlh.lu>
2026-07-16 07:24:49 +0000

    0-fetch_html.py

--- data_analysis/web_scraping/0-fetch_html.py
+++ data_analysis/web_scraping/0-fetch_html.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""a function that fetches a web page and returns its HTML as text"""
+
+import requests
+
+
+def fetch_html(url, headers=None, timeout=10):
+    """ a function that fetches a webpage and returns its HTML"""
+    response = requests.get(url, headers=headers, timeout=timeout)
+
+    if response.status_code >= 400:
+        response.raise_for_status()
+
+    return response.text
