f3f4c6fcb0213c178d80997bb8903b5c9db75877
kysong <kyoungran.song@ai.dlh.lu>
2026-07-17 20:07:15 +0200

    8-scroll_and_scrape.py

--- data_analysis/web_scraping/8-scroll_and_scrape.py
+++ data_analysis/web_scraping/8-scroll_and_scrape.py
@@ -10,6 +10,11 @@ def scroll_and_scrape(url, scroll_pause=2.0):
 
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
 
