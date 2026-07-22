3409900b5356cc5b0ed5bab91e3434c77bf50134
kysong <kyoungran.song@ai.dlh.lu>
2026-07-22 10:22:00 +0200

    0-build.py

--- machine_learning/tree_models/0-build.py
+++ machine_learning/tree_models/0-build.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""create a decision tree classifier using scikit-learn"""
+
+from sklearn import tree
+
+
+def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
+    """create a decision tree classifier using scikit-learn"""
+    model = tree.DecisionTreeClassifier(
+        min_samples_leaf=min_samples_leaf,
+        min_samples_split=min_samples_split,
+        random_state=random_state)
+    return model
