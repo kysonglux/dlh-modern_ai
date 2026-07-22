dab8ae3d398ec040c7461856f372d9cc6378606c
kysong <kyoungran.song@ai.dlh.lu>
2026-07-22 10:22:58 +0200

    1-train.py

--- machine_learning/tree_models/1-train.py
+++ machine_learning/tree_models/1-train.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""train a tree based classifier using scikit-learn"""
+
+
+def train_tree(clf, X, y):
+    """train a tree based classifier using scikit-learn"""
+    clf.fit(X, y)
