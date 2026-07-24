fc08391445c5dd988d95dbc1fff688c2683d9534
kysong <kyoungran.song@ai.dlh.lu>
2026-07-24 18:14:05 +0200

    2-draw.py

--- machine_learning/tree_models/2-draw.py
+++ machine_learning/tree_models/2-draw.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""displays the textual structure of a trained decision tree"""
+
+from sklearn import tree
+
+
+def draw(clf, feature_names, class_names):
+    """displays the textual structure of a trained decision tree """
+    print(tree.export_text(clf,
+                           feature_names=feature_names,
+                           class_names=class_names))
