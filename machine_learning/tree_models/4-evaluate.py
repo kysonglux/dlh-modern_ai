3baa0935378a5a82fd5f07327800d550030c497e
kysong <kyoungran.song@ai.dlh.lu>
2026-07-25 07:21:57 +0200

    4-evaluate.py

--- machine_learning/tree_models/4-evaluate.py
+++ machine_learning/tree_models/4-evaluate.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""generates a detailed classfication report"""
+
+from sklearn import metrics
+
+
+def evaluate(true_labels, predicted_labels, class_names):
+    """generates a detailed classfication report"""
+    return metrics.classification_report(true_labels,
+                                         predicted_labels,
+                                         target_names=class_names)
