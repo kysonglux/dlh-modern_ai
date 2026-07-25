d066251e2091b1021786e1618ec10404bd0329e3
kysong <kyoungran.song@ai.dlh.lu>
2026-07-25 07:31:19 +0200

    6-pruning_path.py

--- machine_learning/tree_models/6-pruning_path.py
+++ machine_learning/tree_models/6-pruning_path.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""retrieves the cost-complexity pruning path"""
+
+
+def get_pruning_path(clf, X, y):
+    """retrieves the costs-complexity pruning path"""
+    clf.fit(X, y)
+
+    path = clf.cost_complexity_pruning_path(X, y)
+
+    ccp_alphas = path.ccp_alphas
+    impurities = path.impurities
+
+    return ccp_alphas, impurities
