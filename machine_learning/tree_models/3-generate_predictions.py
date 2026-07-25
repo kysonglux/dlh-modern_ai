0451e4ef52928e4522ac83c6d9a2c1b09196d88c
kysong <kyoungran.song@ai.dlh.lu>
2026-07-25 07:10:27 +0200

    3-generate_predictions.py

--- machine_learning/tree_models/3-generate_predictions.py
+++ machine_learning/tree_models/3-generate_predictions.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""generate predictions from a trained tree-based classifier"""
+
+import numpy as np
+import pandas as pd
+
+
+def generate_predictions(clf, X):
+    """generate predictions from a trained tree-basaed clssifier"""
+    if not hasattr(clf, "predict"):
+        raise TypeError()
+    if not isinstance(X, (np.ndarray, pd.DataFrame)):
+        raise TypeError()
+    preds = clf.predict(X)
+    return np.asarray(preds)
