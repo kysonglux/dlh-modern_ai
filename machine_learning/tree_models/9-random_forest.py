0992c4f597a2a4caf74fd44320754d1da1ae90d3
kysong <kyoungran.song@ai.dlh.lu>
2026-07-26 19:29:02 +0200

    9-random_forest.py

--- machine_learning/tree_models/9-random_forest.py
+++ machine_learning/tree_models/9-random_forest.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""create a random forest classifier """
+
+from sklearn import ensemble
+
+
+def random_forest(n_estimators, random_state):
+    """create a rondom forest classifier"""
+    model = ensemble.RandomForestClassifier(
+        n_estimators=n_estimators,
+        random_state=random_state
+    )
+    return model
