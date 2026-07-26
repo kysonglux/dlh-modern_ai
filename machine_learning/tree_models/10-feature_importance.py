1481beeffdb0f446bb2c0f96f29db9cd5dd1044b
kysong <kyoungran.song@ai.dlh.lu>
2026-07-26 19:35:12 +0200

    10-feature_importance.py

--- machine_learning/tree_models/10-feature_importance.py
+++ machine_learning/tree_models/10-feature_importance.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""computes and returns the feature importances"""
+
+import numpy as np
+
+
+def feature_importance(rf):
+    """computes and returns the feature importance"""
+    importances = rf.feature_importances_
+
+    indices = np.argsort(importances)
+
+    return importances, indices
