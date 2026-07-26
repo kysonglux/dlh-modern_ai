74e70936026687ed9c11cb3d55eb2742ecd6c5f0
kysong <kyoungran.song@ai.dlh.lu>
2026-07-26 19:50:27 +0200

    11-boosting.py

--- machine_learning/tree_models/11-boosting.py
+++ machine_learning/tree_models/11-boosting.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+"""initializes and returns an untrained boosting classifier """
+
+from sklearn import ensemble
+import xgboost as xgb
+import lightgbm as lgb
+
+
+def compare_boosting_classifiers(name, n_estimators, random_state):
+    """initializes and returns an untrined boosting classifier"""
+    if name == "adaboost":
+        return ensemble.AdaBoostClassifier(
+            n_estimators=n_estimators,
+            random_state=random_state
+        )
+
+    elif name == "gradientboosting":
+        return ensemble.GradientBoostingClassifier(
+            n_estimators=n_estimators,
+            random_state=random_state
+        )
+
+    elif name == "xgboost":
+        return xgb.XGBClassifier(
+            n_estimators=n_estimators,
+            random_state=random_state,
+            use_label_encoder=False,
+            eval_metric="logloss"
+        )
+
+    elif name == "lightgbm":
+        return lgb.LGBMClassifier(
+            n_estimators=n_estimators,
+            random_state=random_state,
+            verbose=-1
+        )
+
+    raise ValueError(f"Unknown model name '{name}'")
