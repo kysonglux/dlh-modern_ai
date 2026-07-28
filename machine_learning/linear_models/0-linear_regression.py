4c1e4b7a6c28c0e12c68595b9ffdd6031fa094fb
kysong <kyoungran.song@ai.dlh.lu>
2026-07-28 08:41:24 +0200

    0-linear_regression.py

---
+++ machine_learning/linear_models/0-linear_regression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""create a linear regression model"""
+
+from sklearn import linear_model
+
+
+def Linear_Regression():
+    """create a linear regression model"""
+    reg = linear_model.LinearRegression()
+    return reg
