3a14845dd49108b8403c3b7437d978d0d489876b
kysong <kyoungran.song@ai.dlh.lu>
2026-08-07 09:42:08 +0200

    2-compile.py

--- deep_learning/dl_with_keras/2-compile.py
+++ deep_learning/dl_with_keras/2-compile.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""compile model and train it"""
+
+from pyexpat import model
+
+from tensorflow import keras
+
+
+def compile_model(model, learning_rate=0.01):
+    """Compile and train the model.
+
+    Args:
+        model (keras.Model): The Keras model to compile and train.
+        learning_rate (float): The learning rate for the optimizer.
+    """
+
+    optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
