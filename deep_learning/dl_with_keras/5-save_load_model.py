f44fafedc0abd39381076c8cc1b84a07ea242e20
kysong <kyoungran.song@ai.dlh.lu>
2026-08-07 11:43:07 +0200

    5-save_load_model.py

--- deep_learning/dl_with_keras/5-save_load_model.py
+++ deep_learning/dl_with_keras/5-save_load_model.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""save and load model"""
+from tensorflow import keras
+
+
+def save_model(network, filename):
+    """Save a model's architecture and weights.
+
+    Args:
+        network (keras.Model): The Keras model to save.
+        filename (str): The path where the model will be saved.
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """Load a model from a file.
+
+    Args:
+        filename (str): The path to the saved model file.
+    """
+    return keras.models.load_model(filename)
