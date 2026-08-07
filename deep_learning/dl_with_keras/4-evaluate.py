db5ea2eb24cb9c4e718e78d826ef2c3c047c0067
kysong <kyoungran.song@ai.dlh.lu>
2026-08-07 10:53:42 +0200

    4-evaluate.py

--- deep_learning/dl_with_keras/4-evaluate.py
+++ deep_learning/dl_with_keras/4-evaluate.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""assess model performance"""
+
+
+def evaluate_model(model, X, Y, verbose=0):
+    """Evaluate the model on the given data.
+
+    Args:
+        model (keras.Model): The Keras model to evaluate.
+        X (numpy.ndarray): The input data for evaluation.
+        Y (numpy.ndarray): The target data for evaluation.
+
+    Returns:
+        float: The loss value.
+        float: The accuracy value.
+    """
+    loss, accuracy = model.evaluate(X, Y, verbose=0)
+    return loss, accuracy
