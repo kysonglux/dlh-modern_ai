cc338c22ed56d41f8ecc119a911ec2aca82dddfe
kysong <kyoungran.song@ai.dlh.lu>
2026-08-07 15:51:39 +0200

    7-predict.py

--- deep_learning/dl_with_keras/7-predict.py
+++ deep_learning/dl_with_keras/7-predict.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""predict using a trained model"""
+
+import tensorflow as tf
+
+
+def predict(model, X, verbose=0):
+    """Predict using a trained model.
+
+    Args:
+        model (keras.Model): The trained Keras model.
+        X (numpy.ndarray): Input data for prediction.
+        verbose (int): Verbosity mode. 0 = silent, 1 = progress bar.
+    """
+    predictions = model.predict(X, verbose=verbose)
+    return predictions
