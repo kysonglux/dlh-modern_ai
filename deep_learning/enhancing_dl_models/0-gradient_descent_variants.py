4b346f11dbb9b4b47306f3239a1bf008a28f18e5
kysong <kyoungran.song@ai.dlh.lu>
2026-08-11 17:38:46 +0200

    0-gradient_descent_variants.py

---
+++ deep_learning/enhancing_dl_models/0-gradient_descent_variants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""return gradient descent optimizer and batch size"""
+
+from tensorflow import keras
+
+
+def train_with_gradient_descent_variant(variant,
+                                        learning_rate, x_train, batch_size):
+    """returns the optimizer and batch size based
+    on the gradient descent variant specified"""
+    if variant == "batch":
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+        bs = x_train.shape[0]
+        return optimizer, bs
+    elif variant == "stochastic":
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+        bs = 1
+        return optimizer, bs
+    elif variant == "mini_batch":
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+        bs = batch_size
+        return optimizer, bs
+    else:
+        raise ValueError("Invalid gradient descent variant")
