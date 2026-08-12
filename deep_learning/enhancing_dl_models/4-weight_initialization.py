54837fce625e214c3486742888fa16d05fef7102
kysong <kyoungran.song@ai.dlh.lu>
2026-08-12 09:45:59 +0200

    4-weight_initialization.py

---
+++ deep_learning/enhancing_dl_models/4-weight_initialization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""return complied keras model based on the specified weight initialization"""
+
+from tensorflow import keras
+
+
+def build_model_initializer_by_activation(input_dim, hidden_units, activation):
+    """returns a compiled keras model
+    based on the specified weight initialization method"""
+    if activation == "sigmoid":
+        initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.05)
+    elif activation == "tanh":
+        initializer = keras.initializers.GlorotNormal()
+    elif activation == "relu":
+        initializer = keras.initializers.HeNormal()
+    elif activation == "leaky_relu":
+        initializer = keras.initializers.HeNormal()
+    else:
+        raise ValueError("Invalid activation function")
+
+    model = keras.Sequential([
+        keras.layers.Dense(hidden_units, activation=activation,
+                           kernel_initializer=initializer,
+                           input_shape=(input_dim,)),
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    return model
