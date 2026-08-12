f49d721747e63daafd59f2ad4c6b8cc4e3e27f1d
kysong <kyoungran.song@ai.dlh.lu>
2026-08-12 11:19:28 +0200

    4-weight_initialization.py

--- deep_learning/enhancing_dl_models/4-weight_initialization.py
+++ deep_learning/enhancing_dl_models/4-weight_initialization.py
@@ -7,30 +7,28 @@ from tensorflow import keras
 def build_model_initializer_by_activation(input_dim, hidden_units, activation):
     """returns a compiled keras model
     based on the specified weight initialization method"""
-    if activation == "sigmoid":
+    if activation in ("sigmoid", "tanh"):
         initializer = keras.initializers.GlorotUniform()
-        activation_layer = keras.layers.Activation("sigmoid")
-    elif activation == "tanh":
-        initializer = keras.initializers.GlorotUniform()
-        activation_layer = keras.layers.Activation("tanh")
-    elif activation == "relu":
+    elif activation in ("relu", "leaky_relu"):
         initializer = keras.initializers.HeNormal()
-        activation_layer = keras.layers.Activation("relu")
-    elif activation == "leaky_relu":
-        initializer = keras.initializers.HeNormal()
-        activation_layer = keras.layers.Activation("leaky_relu")
     else:
         raise ValueError("Invalid activation function")
 
-    model = keras.Sequential([
-        keras.layers.Input(shape=(input_dim,)),
-        keras.layers.Dense(hidden_units,
-                           kernel_initializer=initializer,),
-        activation_layer,
-        keras.layers.Dense(hidden_units,
-                           kernel_initializer=initializer,),
-        activation_layer,
-        keras.layers.Dense(10, activation='softmax')
-    ])
+    model = keras.Sequential()
+    model.add(keras.layers.Input(shape=(input_dim,)))
+    model.add(keras.layers.Dense(hidden_units,
+                                 kernel_initializer=initializer,))
+
+    if activation == "leaky_relu":
+        model.add(keras.layers.LeakyReLU())
+    else:
+        model.add(keras.layers.Activation(activation))
+    model.add(keras.layers.Dense(hidden_units,
+                                 kernel_initializer=initializer,))
+    if activation == "leaky_relu":
+        model.add(keras.layers.LeakyReLU())
+    else:
+        model.add(keras.layers.Activation(activation))
+    model.add(keras.layers.Dense(10, activation='softmax'))
 
     return model
