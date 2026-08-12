eacc7ace806a2a5eb44a2d9760d9e0736ccdcb98
kysong <kyoungran.song@ai.dlh.lu>
2026-08-12 10:50:09 +0200

    4-weight_initialization.py

--- deep_learning/enhancing_dl_models/4-weight_initialization.py
+++ deep_learning/enhancing_dl_models/4-weight_initialization.py
@@ -9,23 +9,27 @@ def build_model_initializer_by_activation(input_dim, hidden_units, activation):
     based on the specified weight initialization method"""
     if activation == "sigmoid":
         initializer = keras.initializers.GlorotUniform()
+        activation_layer = keras.layers.Activation("sigmoid")
     elif activation == "tanh":
         initializer = keras.initializers.GlorotUniform()
+        activation_layer = keras.layers.Activation("tanh")
     elif activation == "relu":
         initializer = keras.initializers.HeNormal()
+        activation_layer = keras.layers.Activation("relu")
     elif activation == "leaky_relu":
         initializer = keras.initializers.HeNormal()
+        activation_layer = keras.layers.Activation("leaky_relu")
     else:
         raise ValueError("Invalid activation function")
 
     model = keras.Sequential([
         keras.layers.Input(shape=(input_dim,)),
         keras.layers.Dense(hidden_units,
-                           activation=activation,
                            kernel_initializer=initializer,),
+        activation_layer,
         keras.layers.Dense(hidden_units,
-                           activation=activation,
                            kernel_initializer=initializer,),
+        activation_layer,
         keras.layers.Dense(10, activation='softmax')
     ])
 
