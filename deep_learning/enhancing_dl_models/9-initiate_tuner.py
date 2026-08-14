b8a84c0bc8414f09584a9d2b294c0dabb7c5d61c
kysong <kyoungran.song@ai.dlh.lu>
2026-08-14 08:57:07 +0200

    9-initiate_tuner.py

---
+++ deep_learning/enhancing_dl_models/9-initiate_tuner.py
@@ -0,0 +1,40 @@
+#! /user/bin/env python3
+"""initialize a keras Tuner for hyperparameter tuning"""
+import keras_tuner as kt
+
+
+def initiate_tuner(tuner_type, build_model, seed,
+                   hyperband_iterations, max_trials,
+                   objective="val_accuracy"):
+    """initialize a keras Tuner for hyperparameter tuning"""
+    if tuner_type == 'Hyperband':
+        tuner = kt.Hyperband(
+            build_model,
+            objective=objective,
+            max_epochs=hyperband_iterations,
+            factor=3,
+            directory='my_dir',
+            project_name='helloworld',
+            overwrite=True
+        )
+    elif tuner_type == 'RandomSearch':
+        tuner = kt.RandomSearch(
+            build_model,
+            objective=objective,
+            max_trials=max_trials,
+            directory='my_dir',
+            project_name='helloworld',
+            overwrite=True
+        )
+    elif tuner_type == 'BayesianOptimization':
+        tuner = kt.BayesianOptimization(
+            build_model,
+            objective=objective,
+            max_trials=max_trials,
+            directory='my_dir',
+            project_name='helloworld',
+            overwrite=True
+        )
+    else:
+        raise ValueError(f"Unknown tuner type: {tuner_type}")
+    return tuner
