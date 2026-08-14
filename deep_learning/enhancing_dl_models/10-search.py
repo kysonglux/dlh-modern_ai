d61e084009866624240583546c5487999a6b6fce
kysong <kyoungran.song@ai.dlh.lu>
2026-08-14 11:14:33 +0000

    10-search.py

---
+++ deep_learning/enhancing_dl_models/10-search.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""retrieve the best hyperparameters from a keras tuner search"""
+
+
+def search_and_return_best_model(tuner, x_train, y_train, epochs,
+                                 validation_split, verbose=0):
+    """retrieve the best hyperparameters from a keras tuner search"""
+    tuner.search(
+        x=x_train,
+        y=y_train,
+        epochs=epochs,
+        validation_split=validation_split,
+        verbose=verbose
+    )
+    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+    return best_hps
