fe5752330b7cead363f6c4adb6bdd263bd6211d4
kysong <kyoungran.song@ai.dlh.lu>
2026-09-01 10:20:40 +0200

    1-classification_head.py

--- deep_learning/transfer_learning/1-classification_head.py
+++ deep_learning/transfer_learning/1-classification_head.py
@@ -6,7 +6,6 @@ from tensorflow import keras
 def add_classification_head(base_model, num_classes):
     """adds a classification head to a base model"""
     x = base_model.output
-    x = keras.layers.GlobalAveragePooling2D()(x)
     x = keras.layers.Dense(1024, activation='relu')(x)
     predictions = keras.layers.Dense(num_classes, activation='softmax')(x)
     model = keras.Model(inputs=base_model.input, outputs=predictions)
