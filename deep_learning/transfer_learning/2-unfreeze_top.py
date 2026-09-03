20601cd82d391d63f86824ab6bfdaeb51cef249a
kysong <kyoungran.song@ai.dlh.lu>
2026-09-03 16:16:53 +0200

    2-unfreeze_top.py

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -2,12 +2,14 @@
 """unfreezes the last N layers of the base model"""
 from tensorflow import keras
 
+
 def unfreeze_top_layers(model, n_layers):
     """unfreezes the last N layers of the base model"""
     if n_layers <= 0:
         raise ValueError("n must be a positive integer")
     if n_layers > len(model.layers):
-        raise ValueError("n must be less than or equal to the number of layers in the base model")
+        raise ValueError("n must be less than or equal "
+                         "to the number of layers in the base model")
 
     # Unfreeze the last N layers
     for layer in model.layers[-n_layers:]:
