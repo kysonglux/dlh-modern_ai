185f5c146fd708de901a5b2a79e1379c0b7b114d
kysong <kyoungran.song@ai.dlh.lu>
2026-09-12 12:21:44 +0200

    1-basic_aug.py

--- deep_learning/cv_apps/1-basic_aug.py
+++ deep_learning/cv_apps/1-basic_aug.py
@@ -2,6 +2,8 @@
 """YOLO-compatible augmentation using Albumentations."""
 import albumentations as A
 import numpy as np
+import cv2
+import random
 
 
 def basic_aug(image, bboxes, labels):
@@ -16,22 +18,33 @@ def basic_aug(image, bboxes, labels):
         aug_bboxes (np.ndarray): Augmented bboxes (VOC format)
         aug_labels (List[int]): Labels (unchanged)
     """
+    random.seed(42)  # For reproducibility
+    np.random.seed(42)
 
     transform = A.Compose(
         [
+            A.PadIfNeeded(
+                min_height=image.shape[0],
+                min_width=image.shape[1],
+                border_mode=cv2.BORDER_CONSTANT,
+                fill=0,
+                p=1.0
+            ),
             A.HorizontalFlip(p=0.5),
             A.RandomBrightnessContrast(p=0.2),
             A.Affine(
                 translate_percent=0.1,
                 scale=0.1,   # scale 0.1 means ±10%
                 rotate=(-30, 0),
+                fit_output=False,
+                interpolation=cv2.INTER_NEAREST,
                 p=0.5
             ),
         ],
         bbox_params=A.BboxParams(
             format="pascal_voc",
             label_fields=["labels"],
-            min_visibility=0.3
+            min_visibility=0.0
         ),
         seed=42
     )
