1589e6e3ae3d38d1d278fe0a63a692f6b668909f
kysong <kyoungran.song@ai.dlh.lu>
2026-09-22 20:50:39 +0200

    6-ngram.py

--- data_analysis/intro_to_nlp/6-ngram.py
+++ data_analysis/intro_to_nlp/6-ngram.py
@@ -7,6 +7,8 @@ def generate_ngrams(tokens, n=2):
     """Generates n-grams from a list of tokens."""
     if not isinstance(tokens, list) or not tokens:
         return []
-    if not isinstance(n, int) or n < 2:
+    if not isinstance(n, int) or n < 1:
         raise ValueError("n must be a positive integer.")
+    if n == 1:
+        return tokens
     return list(map("_".join, nltk.ngrams(tokens, n)))
